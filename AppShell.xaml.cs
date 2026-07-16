f46cfdec5c43d5e8a3a0ff1fab3ae1b0a029c898
jesuslazcano1999uh-blip <jesus.lazcano1999uh@gmail.com>
2026-07-16 08:08:30 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace proyecto_app_maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace proyecto_app_maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
